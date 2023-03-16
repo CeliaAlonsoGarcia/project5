4e2891e1c005fb3e17d81c16144790c2ec5fb9bb
Celia Alonso <celiaag94@gmail.com>
2023-03-16 22:50:10 +0000

    started to work on js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 
+
+// Select all the containers
+const playerChoiceContainer = document.querySelector("#playerChoice");
+const computerChoiceContainer = document.querySelector("#computerChoice");
+const result = document.querySelector(".result");
+const buttons = document.querySelectorAll(".choicetButton");
+
+
+
+// fuction for the player to click on one button and the result to be displayed 
+buttons.forEach()
+
+// inner.html to show the emoji the player has chosen
+function playerChoice(event) {
+    // document.playerChoiceContainer.innerHTML = 
+}
+// computer pics a random emoji
+
+// computer shows the random emoji chosen
+
+// if / else statement to decide who wins
+
+// winning anouncemnt 
