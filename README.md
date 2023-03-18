2173148d28a4596ce235e9bdf876bde38de7a86c
Celia Alonso <celiaag94@gmail.com>
2023-03-18 21:37:11 +0000

    computerchoice is being generated and uploaded

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # project5
 Game project
+
+I couldnt get the user choice to update so I did console.log to make sure the query slectors were working first of all and then the addEventlistener for click was working
+then i realised there was a type when selecting the buttons
--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 
 
 // Select all the containers
-const playerChoiceContainer = document.querySelector("#playerChoice");
-const computerChoiceContainer = document.querySelector("#computerChoice");
-const result = document.querySelector(".result");
-const buttons = document.querySelectorAll(".choicetButton");
+const playerChoiceContainer = document.querySelector("#user-choice");
+console.log(playerChoiceContainer);
+const computerChoiceContainer = document.querySelector("#computer-choice");
+console.log(computerChoiceContainer);
+const result = document.querySelector("#result");
+console.log(result);
+const buttonsChoice = document.querySelectorAll(".choiceButton");
+console.log(buttonsChoice);
+let userChoice
+let computerChoice
+
+
+// fuction for the player to click on one button and the choice to be displayed.
+// when the user clicks on the button we want two things to happen: 1. for the choice of the user
+// to be displayed 2.for the computer to generate a choice
+buttonsChoice.forEach(buttonsChoice => buttonsChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id
+    playerChoiceContainer.innerHTML = userChoice
+    console.log("clicked")
+    generateComputerChoice() 
+
+}))
+
+function generateComputerChoice (){
+    const randomChoice = Math.floor(Math.random() * 3)
+    const computerChoice = buttonsChoice[randomChoice].id;
+    computerChoiceContainer.innerHTML = computerChoice
+}
 
 
 
-// fuction for the player to click on one button and the result to be displayed 
-buttons.forEach()
+
+
 
 // inner.html to show the emoji the player has chosen
-function playerChoice(event) {
+
     // document.playerChoiceContainer.innerHTML = 
-}
+
 // computer pics a random emoji
 
 // computer shows the random emoji chosen
