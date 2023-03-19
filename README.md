1b88a42dd96c91a6d7c848891e4c5d67bab5f979
Celia Alonso <celiaag94@gmail.com>
2023-03-19 15:56:09 +0000

    updated the value to be emoji and now they update

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Game project
 
 I couldnt get the user choice to update so I did console.log to make sure the query slectors were working first of all and then the addEventlistener for click was working
 then i realised there was a type when selecting the buttons
+
+I declared if statement outside a function so it was being called when the page loaded
+
+I wanted to change the words "rock, paper, scissors" for the emoji and i was struggling because I was targetting the id but then added the value to to the buttons to be the emoji and targetted the value instead.
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+  // if statement to update the result announcement
 
 // Select all the containers
 const playerChoiceContainer = document.querySelector("#user-choice");
@@ -11,31 +12,67 @@ const buttonsChoice = document.querySelectorAll(".choiceButton");
 console.log(buttonsChoice);
 let userChoice
 let computerChoice
+let resultDisplay
 
 
 // fuction for the player to click on one button and the choice to be displayed.
-// when the user clicks on the button we want two things to happen: 1. for the choice of the user
-// to be displayed 2.for the computer to generate a choice
+// when the user clicks on the button we want three things to happen: 
+// 1. for the choice of the user to be displayed 
+// 2.for the computer to generate a choice
+// 3. for the result comment to be updated
+
 buttonsChoice.forEach(buttonsChoice => buttonsChoice.addEventListener("click", (e) => {
-    userChoice = e.target.id
+    userChoice = e.target.innerHTML
     playerChoiceContainer.innerHTML = userChoice
     console.log("clicked")
     generateComputerChoice() 
+    updateResult()
 
 }))
 
+// function to generate the random choice
 function generateComputerChoice (){
     const randomChoice = Math.floor(Math.random() * 3)
-    const computerChoice = buttonsChoice[randomChoice].id;
-    computerChoiceContainer.innerHTML = computerChoice
-}
+    computerChoice = buttonsChoice[randomChoice].value;
+    computerChoiceContainer.innerHTML = computerChoice}
+
+    
+function updateResult () {
+
 
 // if statement to update the result announcement
-if (titleInput.value == "" || yearInput.value == "" || plotInput.value == "" || ratingInput.value == "" || runtimeInput.value == "") {
-  alert("Ensure you input a value in all fields!");
-} else {
+    if (computerChoice === userChoice) {result.innerHTML = "It's a draw"}
+    else if (computerChoice === "rock" && userChoice === "scicssors") {result.innerHTML = "Computer wins"}
+    else if (computerChoice === "paper" && userChoice === "rock") {result.innerHTML = "Computer wins"}
+    else if (computerChoice === "scissors" && userChoice === "paper") {result.innerHTML = "Computer wins"}
+    else {result.innerHTML = "You win"}
+    }
+    
 
-}
+
+
+
+
+
+
+// inner.html to show the emoji the player has chosen
+
+    // document.playerChoiceContainer.innerHTML = 
+
+// computer pics a random emoji
+
+// computer shows the random emoji chosen
+
+// if / else statement to decide who wins
+
+// winning anouncemnt {result.innerHTML = "It's a draw"}
+// else if (computerChoice === rock || userChoice === scicssors) {result.innerHTML = "Computer Wins"}
+// else if (computerChoice === paper || userChoice === rock) {result.innerHTML = "Computer Wins"}
+// else if (computerChoice === scissors || userChoice === paper) {result.innerHTML = "Computer Wins"}
+// else {result.innerHTML = "You won!"}
+
+
+
 
 
 
